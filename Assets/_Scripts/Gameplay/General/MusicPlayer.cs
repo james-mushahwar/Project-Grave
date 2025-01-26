c6f12df5bc6f2e42844009f94e8ce6d5c6eb09a6
mu5hjj <james.mushahwar@gmail.com>
2025-01-26 18:27:01 +0000

    classical interactable gramophone added and 3 audio tracks

---
+++ Assets/_Scripts/Gameplay/General/MusicPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Scripts.Gameplay.General.Morgue;
+using _Scripts.Org;
+using UnityEngine;
+
+namespace _Scripts.Gameplay.General{
+    
+    public class MusicPlayer : MorgueActor, IInteractable
+    {
+        [SerializeField] private List<AudioClip> _tracks;
+        [SerializeField] private AudioSource _audioSource;
+        private int _audioIndex = 0;
+
+        [SerializeField] private Animation _playingLoopAnimation;
+
+        public override void EnterHouseThroughChute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ToggleProne(bool set)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Setup()
+        {
+        }
+
+        public override void Tick()
+        {
+        }
+
+        public bool IsInteractable()
+        {
+            return true;
+        }
+
+        public bool OnInteract()
+        {
+            _audioSource.clip = _tracks[_audioIndex];
+            _audioSource.Play(1);
+
+            _audioIndex++;
+            if (_audioIndex >= _tracks.Count)
+            {
+                _audioIndex = 0;
+            }
+
+            _playingLoopAnimation.Play();
+
+            return true;
+        }
+    }
+    
+}
