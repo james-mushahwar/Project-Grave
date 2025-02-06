5dbd323641aef746d22675c180b083607f702568
James Mushahwar <james.mushahwar@gmail.com>
2025-02-06 07:49:08 +0000

    house collision and ui manager WIP

---
+++ Assets/_Scripts/Gameplay/Architecture/Managers/UIManager.cs
@@ -0,0 +1,60 @@
+﻿using _Scripts.CautionaryTalesScripts;
+using Cinemachine;
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Gameplay.Architecture.Managers{
+    
+    public class UIManager : GameManager<UIManager>, IManager
+    {
+        #region Gameplay UI
+        [SerializeField]
+        private Canvas _gameplayCanvas;
+        [SerializeField]
+        private GameObject _gameplayNormalViewGroup;
+        [SerializeField]
+        private GameObject _gameplayOperationViewGroup;
+        #endregion
+
+        // as gamestate is being generated
+        public virtual void ManagedPreInitialiseGameState() { }
+        // after gamestate is generated
+        public virtual void ManagedPostInitialiseGameState() 
+        {
+            if (PlayerManager.Instance.CurrentPlayerController.PlayerControllerState == Player.Controller.EPlayerControllerState.Operating)
+            {
+                _gameplayOperationViewGroup.SetActive(true);
+            }
+        }
+        // before main menu loads
+        public virtual void ManagedPreMainMenuLoad() { }
+        // after main menu loads
+        public virtual void ManagedPostMainMenuLoad() { }
+        // before world (level, area, zone) starts loading
+        public virtual void ManagedPreInGameLoad() { }
+        // after world (level, area, zone) finished loading
+        public virtual void ManagedPostInGameLoad() { }
+        // save states are restored
+        public virtual void ManagedRestoreSave() { }
+        // after save states are restored
+        public virtual void ManagedPostRestoreSave() { }
+        // before play begins 
+        public virtual void ManagedPrePlayGame() { }
+
+        // tick for playing game 
+        public virtual void ManagedTick() { }
+        // late update tick for playing game 
+        public virtual void ManagedLateTick() { }
+
+        public virtual void ManagedFixedTick() { }
+
+        // before world (level, area, zone) starts unloading
+        public virtual void ManagedPreTearddownGame() { }
+        // after world (level, area, zone) unloading
+        public virtual void ManagedPostTearddownGame() { }
+
+    }
+
+}
